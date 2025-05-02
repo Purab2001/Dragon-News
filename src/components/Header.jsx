4bb89c82fc8339015c522bff75c21327a2773acf
Abir Shahadat Purab <a.s.purab0@gmail.com>
2025-05-02 20:40:33 +0600

    fix: update Header component styles for improved layout and readability

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,10 @@ import { format } from 'date-fns';
 const Header = () => {
     return (
         <div className='flex flex-col justify-center items-center gap-3 pt-12'>
-            <img src={logo} alt="" className='max-w-sm md:max-w-full' />
+            <img src={logo} alt="" className='max-w-80 md:max-w-full' />
             <p className='text-accent'>Journalism Without Fear or Favour</p>
             <div className='font-semibold flex gap-1'>
-                <span className='text-black'>{format(new Date(), "EEEE")},</span>
+                <span className='text-primary'>{format(new Date(), "EEEE")},</span>
                 <span className='text-accent'>{format(new Date(), "MMMM dd, yyyy")}</span>
             </div>
         </div>
